0086cd4b15b9659b89b7750dd4915ec1499e9140
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-02 13:28:15 +0530

    Prog.C

---
+++ Prog.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+clrscr();
+printf("Enter the number");
+scanf("%d",&a);
+if((a%2)=0)
+printf("The given number is even");
+else
+printf("The given number is odd");
+getch();
+return 0;
+}
